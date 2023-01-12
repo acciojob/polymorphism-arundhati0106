bcbe242b1104149e76fe0f3bb7a76bc9d7cb62aa
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-01-12 23:55:54 +0530

    version 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String [] args) {
+        Product p = new Product();
+        p.product(101,22);
+        p.product(101,22, 33);
+        p.product(101.5,22.76);
+    }
+}
 
+class Product {
+    public int product(int x, int y) {
+        return x*y;
+    }
+
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+
+    public double product(double x, double y) {
+        return x*y;
+    }
 }
